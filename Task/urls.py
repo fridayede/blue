fb25af3db5d40ad588fd4ebc5a694b0775ef7b54
collins <edepaul148@gmail.com>
2026-06-26 14:45:11 +0100

    nnn

--- Task/urls.py
+++ Task/urls.py
@@ -6,7 +6,9 @@ app_name ='Task'
 urlpatterns = [
     path('hometask/',views.hometask, name='hometask'),  
     path("task/<int:id>/", views.task, name="task"),
-    path('complect/', views.complect, name='complect'), 
+    path('complect/', views.complect, name='complect'),
+    path('get-ad-token/', views.request_ad_token, name='get_ad_token'),
+    path('adsgram-postback/', views.adsgram_postback, name='adsgram_postback'),
     
 ]
 # path('details/<int:product_id>/',views.product_detail, name="product_detail")
