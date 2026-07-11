d414b3e78c183b7f136cb186e465eb07a71cc0d7
collins <edepaul148@gmail.com>
2026-07-11 13:56:56 +0100

    movint

--- Task/views.py
+++ Task/views.py
@@ -341,6 +341,9 @@ import json
 MAX_ADS_PER_DAY = 30          # you can change this number anytime
 @login_required
 def request_ad_token(request):
+    print(request.user)
+    print("Requesting ad token for user:", request.user)
+    print(user)
     user = request.user
     today = date.today()
 
