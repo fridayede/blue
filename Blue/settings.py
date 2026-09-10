11a85ec9ba65022d0c4ab00ef54373b01334fef5
collins <edepaul148@gmail.com>
2026-09-10 10:56:41 +0100

    message

--- Blue/settings.py
+++ Blue/settings.py
@@ -26,9 +26,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-ALLOWED_HOSTS = ['YOUR-BACK4APP-DOMAIN.back4app.io', 'localhost', '127.0.0.1']
 
-CSRF_TRUSTED_ORIGINS = ['https://YOUR-BACK4APP-DOMAIN.back4app.io']
+
+
+
+ALLOWED_HOSTS = ['blue-ypsn3bof.b4a.run', 'localhost', '127.0.0.1']
+
+CSRF_TRUSTED_ORIGINS = ['https://blue-ypsn3bof.b4a.run']
 
 # ALLOWED_HOSTS = ['blue-25bdc.containers.snapdeploy.app', 'localhost', '127.0.0.1']
 
